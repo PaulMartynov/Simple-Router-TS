32437cbbad06b4d89d7830557431ed1bab0a0ad3
PaulMartynov <juanfen@yandex.ru>
2021-09-06 22:36:00 +0300

    первое приближение

--- src/Types.d.ts
+++ src/Types.d.ts
@@ -1,2 +1,9 @@
 type functionPath = (path: string) => boolean;
 type Route = string | RegExp | functionPath;
+
+interface Listener {
+  route: Route;
+  onEnter?: any;
+  onLeave?: any;
+  beforeEnter?: any;
+}
--- src/router.ts
+++ src/router.ts
@@ -1,17 +1,62 @@
 export class Router {
-  private listeners = [];
+  private listeners: Map<Route, any> = new Map<Route, any>();
 
   private currentPath: string;
 
-  private previousPath: string | null;
+  private previousPath: string;
 
   constructor() {
     this.currentPath = window.location.pathname;
     this.previousPath = this.currentPath;
+    window.addEventListener("popstate", this.handleAllListeners);
   }
 
   private isMatch = (match: Route, path: string) =>
     (match instanceof RegExp && match.test(path)) ||
     (typeof match === "function" && match(path)) ||
     (typeof match === "string" && match === path);
+
+  private handleListener = async (listener: Listener) => {
+    const args = {
+      currentPath: this.currentPath,
+      previousPath: this.previousPath,
+      state: window.history.state,
+    };
+
+    const { route, onEnter, onLeave, beforeEnter } = listener;
+
+    if (this.isMatch(route, this.currentPath) && beforeEnter) {
+      await beforeEnter(args);
+    }
+
+    if (this.isMatch(route, this.currentPath) && onEnter) {
+      await onEnter(args);
+    }
+    if (this.isMatch(route, this.previousPath) && onLeave) {
+      await onLeave(args);
+    }
+  };
+
+  private handleAllListeners = () =>
+    this.listeners.forEach(this.handleListener);
+
+  on = async (
+    route: Route,
+    onEnter: any,
+    onLeave: any,
+    beforeEnter: any
+  ): Promise<void> => {
+    const listener = { route, onEnter, onLeave, beforeEnter };
+    this.listeners.set(route, listener);
+    await this.handleListener(listener);
+    this.listeners.delete(route);
+  };
+
+  go = (url: string, state: any): void => {
+    this.previousPath = this.currentPath;
+    window.history.pushState(state, url, url);
+    this.currentPath = window.location.pathname;
+
+    this.handleAllListeners();
+  };
 }
